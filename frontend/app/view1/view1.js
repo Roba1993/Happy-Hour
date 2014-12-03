f55a14449b8a153583b1bbc1b37c23f736922e97
Markus Thömmes <markusthoemmes@me.com>
2014-12-03 14:02:28 +0000

    Grundlegende Tests zu Angular und Design

--- frontend/app/view1/view1.js
+++ frontend/app/view1/view1.js
@@ -14,5 +14,25 @@ angular.module('myApp.view1', ['ngRoute'])
 // Definiere den Controller
 .controller('View1Ctrl', ['$scope', function($scope) {
   // Setze einen Wert in den Scope (um ihn im view1.html anzuzeigen)
-  $scope.testValue = 'Welt';
+  $scope.barSchalter = false;
+  $scope.bars = [];
+  var bar1 = {
+  	name: 'Monobar',
+  	description: 'Eine tolle Bar',
+  	rating: 4
+  };
+  $scope.bars.push(bar1);
+  var bar2 = {
+  	name: 'Tequila Bar',
+  	description: 'Eine tolle Bar mit tollen Getränken',
+  	rating: 5
+  };
+  $scope.bars.push(bar2);
+  var bar3 = {
+  	name: 'Lutscher Bar',
+  	description: 'Eine Bar mit hartem Lörres',
+  	rating: 1
+  };
+  $scope.bars.push(bar3);
+
 }]);
