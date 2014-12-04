55cd1bbd978d761ed10e73af9bc0114a10684426
Markus Thömmes <markusthoemmes@me.com>
2014-12-04 16:08:59 +0000

    Erste Unit-Tests für RoutesPersistence, #56

--- frontend/test/components/persistence/RoutesPersistenceTest.js.js
+++ frontend/test/components/persistence/RoutesPersistenceTest.js.js
@@ -1,11 +1,55 @@
 describe('happyHour.persistence.RoutesPersistence module', function() {
+  var store = {};
+  beforeEach(function() {
+    store = {};
 
-  beforeEach(module('happyHour.persistence.RoutesPersistence'));
+    module('happyHour.persistence.RoutesPersistence');
+    module(function ($provide) {
+      $provide.value('localStorageService', {
+        set: function(key, value) {
+          store[key] = value;
+        },
+        get: function(key) {
+          if(store[key] === undefined) {
+            return null;
+          }
+          return store[key];
+        }
+      });
+    });
+  });
 
   describe('RoutesPersistenceService', function(){
 
     it('sollte Routen hinzufügen', inject(function(RoutesPersistenceService) {
       expect(RoutesPersistenceService).toBeDefined();
+
+      RoutesPersistenceService.add({id:1});
+      expect(store.routes.length).toBe(1);
+
+      RoutesPersistenceService.add({id:2});
+      expect(store.routes.length).toBe(2);
+    }));
+
+    it('sollte keine Routen zu voller Liste hinzufügen', inject(function(RoutesPersistenceService) {
+      expect(RoutesPersistenceService).toBeDefined();
+
+      RoutesPersistenceService.add({id:1});
+      RoutesPersistenceService.add({id:1});
+      RoutesPersistenceService.add({id:1});
+      RoutesPersistenceService.add({id:1});
+      RoutesPersistenceService.add({id:1});
+      RoutesPersistenceService.add({id:1});
+      RoutesPersistenceService.add({id:1});
+      RoutesPersistenceService.add({id:1});
+      RoutesPersistenceService.add({id:1});
+      RoutesPersistenceService.add({id:1});
+
+
+      expect(store.routes.length).toBe(10);
+
+      expect(RoutesPersistenceService.add({id:2})).toBe(false);
+      expect(store.routes.length).toBe(10);
     }));
 
   });
