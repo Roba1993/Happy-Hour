79ff6ac743e966ce6a1ecee4d25f1718119b018f
Dorothee Nies <dorotheenies@gmx.de>
2014-12-05 10:44:30 +0100

    Unit tests abgeschlossen, betrifft #56

--- frontend/test/components/persistence/RoutesPersistenceTest.js
+++ frontend/test/components/persistence/RoutesPersistenceTest.js
@@ -1,3 +1,7 @@
+/**
+ * Tests für das RoutesPersistence Objekt
+ * @author Dorothee Nies
+ */
 describe('happyHour.persistence.RoutesPersistence module', function() {
   var store = {};
   beforeEach(function() {
@@ -50,6 +54,110 @@ describe('happyHour.persistence.RoutesPersistence module', function() {
 
       expect(RoutesPersistenceService.add({id:2})).toBe(false);
       expect(store.routes.length).toBe(10);
+    }));
+	
+	it('sollte Routen löschen', inject(function(RoutesPersistenceService) {
+      expect(RoutesPersistenceService).toBeDefined();
+
+      RoutesPersistenceService.add({id:1});
+	  RoutesPersistenceService.add({id:2});
+	  RoutesPersistenceService.remove(1);
+      expect(store.routes.length).toBe(1);
+	  
+	}));
+	  
+	it('sollte nicht aus leerer Liste löschen', inject(function(RoutesPersistenceService) {
+      expect(RoutesPersistenceService).toBeDefined();
+	  
+	  var test1 = RoutesPersistenceService.remove(1);
+	  expect(test1).toBe(false);
+	}));
+	  
+	it('sollte keine nicht vorhandenen Routen löschen', inject(function(RoutesPersistenceService) {
+	  RoutesPersistenceService.add({id:1});
+	  RoutesPersistenceService.add({id:2});
+	  expect(store .routes.length).toBe(2);
+	  
+	  var test2 = RoutesPersistenceService.remove(3);
+	  expect(test2).toBe(false);
+	  expect(store .routes.length).toBe(2);
+	  
+	  
+    }));
+	
+	it('sollte Routen zurückgeben', inject(function(RoutesPersistenceService) {
+      expect(RoutesPersistenceService).toBeDefined();
+
+      RoutesPersistenceService.add({id:1});
+	  var test1 = RoutesPersistenceService.get(1);
+      expect(test1.id).toBe(1);
+
+    }));
+	
+	it('sollte keine nicht vorhandenen Routen zurückgeben', inject(function(RoutesPersistenceService) {
+      expect(RoutesPersistenceService).toBeDefined();
+
+      RoutesPersistenceService.add({id:1});
+	  var test1 = RoutesPersistenceService.get(3);
+      expect(test1).toBe(null);
+
+    }));
+	
+	it('sollte alle vorhandenen Routen zurückgeben', inject(function(RoutesPersistenceService) {
+      expect(RoutesPersistenceService).toBeDefined();
+
+      RoutesPersistenceService.add({id:1});
+	  RoutesPersistenceService.add({id:3});
+	  RoutesPersistenceService.add({id:8});
+	  RoutesPersistenceService.add({id:7});
+	  RoutesPersistenceService.add({id:2});
+	  var test1 = RoutesPersistenceService.getAll();
+	  var ids = [1, 3, 8, 7, 2];
+	  for(var i = 0; i < test1.length; i++) {
+		expect(test1[i].id).toBe(ids[i]);
+	  }
+
+    }));
+	
+	it('sollte ein leeres Array zurückgeben wenn keine Routen verfügbar sind', inject(function(RoutesPersistenceService) {
+      expect(RoutesPersistenceService).toBeDefined();
+
+      var test1 = RoutesPersistenceService.getAll();
+	  expect(test1.length).toBe(0);
+
+    }));
+	
+	it('sollte true zurückgeben wenn die Liste voll ist', inject(function(RoutesPersistenceService) {
+      expect(RoutesPersistenceService).toBeDefined();
+	
+	  RoutesPersistenceService.add({id:1});
+	  RoutesPersistenceService.add({id:3});
+	  RoutesPersistenceService.add({id:8});
+	  RoutesPersistenceService.add({id:7});
+	  RoutesPersistenceService.add({id:2});
+	  RoutesPersistenceService.add({id:1});
+	  RoutesPersistenceService.add({id:3});
+	  RoutesPersistenceService.add({id:8});
+	  RoutesPersistenceService.add({id:7});
+	  RoutesPersistenceService.add({id:2});
+	  
+	  var test1 = RoutesPersistenceService.isFull();
+	  
+	  expect(test1).toBe(true);
+
+    }));
+	
+	it('sollte false zurückgeben wenn die Liste nicht voll ist', inject(function(RoutesPersistenceService) {
+      expect(RoutesPersistenceService).toBeDefined();
+	
+	  RoutesPersistenceService.add({id:1});
+	  RoutesPersistenceService.add({id:3});
+
+	  
+	  var test1 = RoutesPersistenceService.isFull();
+	  
+	  expect(test1).toBe(false);
+
     }));
 
   });
