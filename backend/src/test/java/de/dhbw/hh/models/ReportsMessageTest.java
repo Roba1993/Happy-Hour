098d1be6e5a4a621938af0ac9b16721898350027
Jonas Stegmaier <jonas.stegmaier@gmail.com>
2014-12-05 15:51:12 +0100

    Erste Version der Reports GET Anfrage erstellt.

---
+++ backend/src/test/java/de/dhbw/hh/models/ReportsMessageTest.java
@@ -0,0 +1,20 @@
+package de.dhbw.hh.models;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * In dieser Klasse kannst du die modifizierten Getter&Setter
+ * Funktionen der Datenklasse testen. Normale Getter&Setter
+ * Methoden brauchen nicht getestet zu werden.
+ */
+public class ReportsMessageTest {
+
+	@Test
+    public void testExample() throws Exception {
+        // Teste hier spezielle Getter&Setter
+        assertTrue(true);
+    }
+	
+}
