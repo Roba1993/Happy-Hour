4764340c68950561c1f0f029398c7fe362cd6ea7
Patrick Puritscher <blackout289@gmail.com>
2014-12-18 15:51:28 +0100

    added backside to listelement

--- design/template/js/demo.js
+++ design/template/js/demo.js
@@ -2,7 +2,7 @@
  * Diese Datei dient nur zur Demostration einiger Funktionen
  */ 
 $(document).ready(function() {	
-	$('#theatre-recentroute').find('.e__list-big-element').on('click',function() {
-		$(this).toggleClass('is__expanded');
+	$('#theatre-recentroute').find('.e__list-big-element-frontside').on('click',function() {
+		$(this).parents('.e__list-big-element').toggleClass('is__expanded');
 	});
 });
