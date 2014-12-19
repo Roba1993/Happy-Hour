56e8e79986e3c03bd2e606ba09b8d8c5d48997d5
Markus Thömmes <markusthoemmes@me.com>
2014-12-19 08:25:33 +0000

    Codedokumentation von RatingDirective und TimeFilter

--- frontend/app/components/filters/TimeFilter.js
+++ frontend/app/components/filters/TimeFilter.js
@@ -1,8 +1,15 @@
+/**
+ * Erstellt aus einem Objekt, das `startTime` und `endTime` enthält ein lesbares Zeitraumformat
+ *
+ * @author Dorothee Nies
+ * @param {Object} Ein Objekt mit den Variablen `startTime` und `endTime`
+ * @return {String} Ein Zeitraum des Formats "hh:mm - hh:mm" beziehungsweise "ab hh:mm" für einen Zeitraum ohne definiertes Ende.
+ */
 angular.module('happyHour.filters.TimeFilter', [])
 	.filter('formatTime', function() {
 		return function(input) {
-		
-			if (input.endTime === null){
+			// Open-End wenn endTime = null
+			if (input.endTime === null) {
 				return 'ab ' + input.startTime;
 			}
 			else {
