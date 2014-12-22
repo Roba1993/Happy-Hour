b85ec4980193eabc33a9e1fec6a11dbfa27e72a6
Markus Thömmes <markusthoemmes@me.com>
2014-12-22 11:52:39 +0000

    DayPickerDirective Unit Tests abgeschlossen, betrifft #89

--- frontend/app/components/directives/DayPickerDirective.js
+++ frontend/app/components/directives/DayPickerDirective.js
@@ -17,7 +17,10 @@ angular.module('happyHour.directives.DayPickerDirective', [])
 			template: '<div ng-click="previousClicked()" class="col-1"><h4>&lt;</h4></div><div class="col-10" style="text-align:center;"><h4 style="color: #fff">{{result | formatDays:true}}</h4></div><div ng-click="nextClicked()" class="col-1" style="text-align:right;"><h4>&gt;</h4></div>',
 			link: function ($scope) {
 				if($scope.result === undefined) {
-					$scope.result = new Date().getDay()+1;
+					$scope.result = new Date().getDay();
+					if($scope.result === 0) {
+						$scope.result = 7;
+					}
 				}
 
 				$scope.previousClicked = function() {
