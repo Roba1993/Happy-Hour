b43fe4307a4e775bee643f5498aadb2048bc316f
Patrick Puritscher <blackout289@gmail.com>
2015-01-23 16:12:20 +0100

    trigger sidebar via JS

--- design/template/js/demo.js
+++ design/template/js/demo.js
@@ -5,4 +5,7 @@ $(document).ready(function() {
 	$('#theatre-recentroute').find('.e__list-big-element-frontside').on('click',function() {
 		$(this).parents('.e__list-big-element').toggleClass('is__expanded');
 	});
+	$('#trigger-sidebar').on('click', function() {
+		$('#main').toggleClass('sidebar-is-open');
+	});
 });
