5a277f5d5f8692b6115c66cfa92a95a50e6e3937
Neram <wi12180@lehre.dhbw-stuttgart.de>
2015-01-26 13:42:40 +0100

    Bilder geändert

--- backend/src/test/java/de/dhbw/hh/models/BarTest.java
+++ backend/src/test/java/de/dhbw/hh/models/BarTest.java
@@ -4,8 +4,16 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * Diese Klasse testet die Datenklasse Bar
+ * 
+ * @author Tobi
+ */
 public class BarTest {
 
+	/**
+	 * Diese Funktion testet den erweitereten Konstruktor der Klasse Bar
+	 */
 	@Test
 	public void test() {
 		assertEquals(0,0);
